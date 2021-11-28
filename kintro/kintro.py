5f63b991df3cae9091c87a230beb71a17875f0e3
Leonidas Loucas <merc1031@gmail.com>
2021-11-28 02:28:45 +0000

    Allow setting log level

--- kintro/kintro.py
+++ kintro/kintro.py
@@ -2,15 +2,29 @@ from kintro.connect import account, server
 from kintro.utils import _init_logger
 
 import click
+import logging
 
 
 @click.group()
 @click.version_option()
+@click.option(
+    '--log-level',
+    default='info',
+    type=click.Choice(['debug', 'info', 'warning', 'error']),
+    help='Set the minimum log level'
+)
 @click.pass_context
-def cli(ctx):
+def cli(ctx, log_level):
+
+    log_level = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+    }[log_level.lower()]
 
     ctx.obj = {}
-    ctx.obj['logger'] = _init_logger('kintro')
+    ctx.obj['logger'] = _init_logger('kintro', log_level)
 
 cli.add_command(account)
 cli.add_command(server)
